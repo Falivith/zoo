8727c61bfd76a3c6a14da485a22fd6596892f55d
Falivith <falivith@gmail.com>
2023-04-04 08:45:12 -0300

    First commit via WSL

--- zoo.cpp
+++ zoo.cpp
@@ -1,5 +1,29 @@
 #include <fstream>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void threadFunction(int id) {
+	cout << "Thread " << id << " started" << endl;
+	for(int i = 0; i < 10; i++){
+		cout << i << endl;
+	}
+	cout << "Thread " << id << " ended" << endl;
+}
 
 int main () {
+	const int N_THREADS = 4;
+       	thread threads[N_THREADS];
+
+	for(int i = 0; i < N_THREADS; i++) {
+		threads[i] = thread(threadFunction, i);
+	}
+
+	for(int i = 0; i < N_THREADS; i++) {
+		threads[i].join();
+	}
+
+	cout << "All the threads ended." << endl;
     return 0;
 }
